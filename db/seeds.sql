bd63d6a27925cb94b1fb8b65a4eaae1edc649955
Tori <torimpham@gmail.com>
2020-02-22 12:28:52 -0800

    created db schema sql seeds sql and config folder

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES (
+    "Five Dollar Burger"
+)
+INSERT INTO burgers (burger_name) VALUES (
+    "Sourdough Jack"
+)
+INSERT INTO burgers (burger_name) VALUES (
+    "Double Double"
+)
