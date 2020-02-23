717163ec6dad0eeeb6d6b2569bac4daad0d0eabb
Tori <torimpham@gmail.com>
2020-02-22 16:36:04 -0800

    arranged the file system

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(condition, cb) {
+        orm.put("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
