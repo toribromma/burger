f0dc923a9249e9b0cb5ec5724e8dbfe95e285e96
Tori <torimpham@gmail.com>
2020-02-25 18:32:35 -0800

    added lasers

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,4 +1,3 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
